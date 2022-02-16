46b579ac0d34beab96c20ac638488ee790ab41ab
bbbbruno <bbbbruno69@gmail.com>
2022-02-16 13:59:14 +0900

    誕生日から星座を求めるプログラムを作成

---
+++ basics/seiza.ml
@@ -0,0 +1,30 @@
+(* 誕生日（月と日）を受け取ったら、星座を返す *)
+(* int -> int -> string *)
+let seiza month day =
+  if (month = 3 && day >= 21) || (month = 4 && day <= 19) then "牡羊座"
+  else if (month = 4 && day >= 20) || (month = 5 && day <= 20) then "牡牛座"
+  else if (month = 5 && day >= 21) || (month = 6 && day <= 21) then "双子座"
+  else if (month = 6 && day >= 22) || (month = 7 && day <= 22) then "蟹座"
+  else if (month = 7 && day >= 23) || (month = 8 && day <= 22) then "獅子座"
+  else if (month = 8 && day >= 23) || (month = 9 && day <= 22) then "乙女座"
+  else if (month = 9 && day >= 23) || (month = 10 && day <= 23) then "天秤座"
+  else if (month = 10 && day >= 24) || (month = 11 && day <= 22) then "蠍座"
+  else if (month = 11 && day >= 23) || (month = 12 && day <= 21) then "射手座"
+  else if (month = 12 && day >= 22) || (month = 1 && day <= 19) then "山羊座"
+  else if (month = 1 && day >= 20) || (month = 2 && day <= 18) then "水瓶座"
+  else if (month = 2 && day >= 19) || (month = 3 && day <= 20) then "魚座"
+  else ""
+
+(* テスト *)
+let test1 = seiza 4 13 = "牡羊座"
+let test2 = seiza 4 20 = "牡牛座"
+let test3 = seiza 6 18 = "双子座"
+let test4 = seiza 6 30 = "蟹座"
+let test5 = seiza 7 25 = "獅子座"
+let test6 = seiza 9 22 = "乙女座"
+let test7 = seiza 10 1 = "天秤座"
+let test8 = seiza 11 12 = "蠍座"
+let test9 = seiza 12 8 = "射手座"
+let test10 = seiza 12 25 = "山羊座"
+let test11 = seiza 2 14 = "水瓶座"
+let test12 = seiza 2 20 = "魚座"
